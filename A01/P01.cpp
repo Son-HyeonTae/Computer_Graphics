a5df8c037b263159c32d14b1f748e413c047cb27
Son Hyeon Tae <104341400+Son-HyeonTae@users.noreply.github.com>
2022-10-26 22:02:38 +0900

    Assignment 01

--- A01/P01.cpp
+++ A01/P01.cpp
@@ -15,7 +15,7 @@ void Display() {
 
 int main(int argc, char* argv[]) {
 	glutInit(&argc, argv); // GLUT 라이브러리 초기화
-	glutCreateWindow("Hello World!"); // 윈도우 생성 (윈도우 이름 : Hello World!)
+	glutCreateWindow("Hello World!"); // 윈도우 생성 (이름 : Hello World!)
 	glutDisplayFunc(Display); // 출력 함수의 지정
 	glutMainLoop(); // 이벤트 처리 루프
 	return 0; // main 함수 return
